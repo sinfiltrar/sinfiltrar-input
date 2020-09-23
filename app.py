cce8f3afb8f1664d55f2628648fed005da8f365d
Mauro Spivak <mauros@gmail.com>
2020-09-23 11:19:12 -0300

    Initial commit

--- app.py
+++ app.py
@@ -16,20 +16,16 @@ def handle_sns_message(event):
     snsData = json.loads(event.message)
     app.log.debug("Received message with subject: %s, message: %s", event.subject, event.message)
 
-    app.log.debug('SNS DATA %s', snsData)
+    app.log.debug('SNS DATA %s', snsData['mail']['commonHeaders'])
 
-    object = s3.Object(data['receipt']['action']['bucketName'], data['receipt']['action']['objectKey'])
-    app.log.debug('Got body from %s', data['receipt'])
+    object = s3.Object(snsData['receipt']['action']['bucketName'], snsData['receipt']['action']['objectKey'])
 
     mailBody = object.get()['Body'].read().decode('utf-8')
+    app.log.debug('Got body from %s/%s', snsData['receipt']['action']['bucketName'], snsData['receipt']['action']['objectKey'])
 
-    parsedData = {
-        "from":
-        "to":
-        "subject":
-        "body":
-    }
+    parsedData = snsData['mail']['commonHeaders']
 
+    parsedData['body'] = mailBody
 
     r = requests.post(url, json = parsedData, headers = { 'Content-Type': 'application/json' })
     app.log.debug(r.status_code)
