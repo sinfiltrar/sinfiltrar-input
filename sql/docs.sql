39fd227d8f3124b80067ae5e0285c3f8f88f271e
Mauro Spivak <mauros@gmail.com>
2020-09-25 19:59:48 -0300

    Docs table

---
+++ sql/docs.sql
@@ -0,0 +1,14 @@
+CREATE TABLE docs
+(
+    id varchar(255) PRIMARY KEY,
+    from_email varchar(255),
+    slug varchar(255),
+    subject varchar(255),
+    shorttext varchar(255),
+    body text,
+    body_plain text,
+    attachments jsonb,
+    meta jsonb,
+    received_at timestamp,
+    created_at timestamp NOT NULL DEFAULT current_timestamp
+);
