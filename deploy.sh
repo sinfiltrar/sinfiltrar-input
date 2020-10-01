d6eb52af852193b89ff5cd1593a2260b6df826fe
Iván Raskovsky <raskovsky+git@gmail.com>
2020-10-01 18:46:07 -0300

    add deploy command and api custom domain name

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+OUTPUT=`chalice deploy --connection-timeout 6000 --no-autogen-policy`
+# OUTPUT=`cat deploy.output`
+
+HOSTED_ZONE=`echo "$OUTPUT" | awk '/HostedZoneId: ([0-9A-Z]+)/ { print $2 }'`
+DOMAIN_NAME=`echo "$OUTPUT" | awk '/AliasDomainName: (.+)/ { print $2 }'`
+
+# echo $HOSTED_ZONE
+# echo $DOMAIN_NAME
+
+aws route53 change-resource-record-sets --hosted-zone-id Z0832787YMKJ27WDQ5UT --change-batch \
+'{
+  "Changes": [
+    {
+      "Action": "UPSERT",
+      "ResourceRecordSet": {
+        "Name": "api.sinfiltr.ar",
+        "Type": "A",
+        "AliasTarget": {
+          "DNSName": "'"$DOMAIN_NAME"'",
+          "HostedZoneId": "'"$HOSTED_ZONE"'",
+          "EvaluateTargetHealth": false
+        }
+      }
+    }
+  ]
+}'
+
+# FIXME: not safe!! sql like injection possible
