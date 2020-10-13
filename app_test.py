d52fa742b23240bde371b78af8df948f690d6bcc
Mauro Spivak <mauros@gmail.com>
2020-10-13 14:37:24 -0300

    Let`s test emails better. One test per issue

--- app_test.py
+++ app_test.py
@@ -1,14 +1,31 @@
 from chalice.test import Client
 from app import app
+import json
 
 
-with open('example_sns_message_embeded.json', 'r') as file:
-    message = file.read()
+def test_email_1():
+    payload = sns_handler_test('tgc9bleng0asjn49k6u2336kn9dh6838siljg1g1')
+    assert payload == None
 
-def test_sns_handler():
+def test_email_2():
+    payload = sns_handler_test('igifg6nk4gvu27enf2rqng6217ka1h8qa8lbj1o1')
+    assert payload == None
+
+def sns_handler_test(object, bucket = 'sinfiltrar-input'):
+
+    message = {
+        "receipt": {
+            "action": {
+                "bucketName": bucket,
+                "objectKey": object
+            }
+        }
+    }
+
     with Client(app) as client:
         response = client.lambda_.invoke(
             "handle_sns_message",
-            client.events.generate_sns_event(message=message)
+            client.events.generate_sns_event(message=json.dumps(message))
         )
-        assert response.payload == None
+
+        return response.payload
